67fac48b8bed3e53dbddb80b3bab89d1e13cfa69
Juppie902 <Juppie902@gmail.com>
2022-09-24 21:59:02 +0300

    Switched to selenium chrome service (old method deprecated) Commented out email validation (not working with debugging)

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from linkedineasyapply import LinkedinEasyApply
-from validate_email import validate_email
+# import validate_email
+from selenium.webdriver.chrome.service import Service
 
 def init_browser():
     browser_options = Options()
@@ -13,7 +14,7 @@ def init_browser():
     for option in options:
         browser_options.add_argument(option)
 
-    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=browser_options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=browser_options)
 
     driver.set_window_position(0, 0)
     driver.maximize_window()
@@ -36,7 +37,7 @@ def validate_yaml():
         if mandatory_param not in parameters:
             raise Exception(mandatory_param + ' is not inside the yml file!')
 
-    assert validate_email(parameters['email'])
+    # assert validate_email(parameters['email'])
     assert len(str(parameters['password'])) > 0
 
     assert isinstance(parameters['disableAntiLock'], bool)
